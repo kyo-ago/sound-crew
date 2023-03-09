c32149534bea6b4235bb9dcc09313a978601d699
kyo-ago <kyo.ago@gmail.com>
2023-03-09 09:12:01 +0900

    change volume range

--- src/pages/Meeting.tsx
+++ src/pages/Meeting.tsx
@@ -2,10 +2,16 @@ import { useJoinZoom } from "../hooks/useJoinZoom";
 import { ZoomJoinParams } from "../libs/zoomClient";
 import { useRecoilValue } from "recoil";
 import { soundContextState } from "../libs/soundContext";
+import { useState } from "react";
 
 export const Meeting = (zoomJoinParams: ZoomJoinParams) => {
   const soundContext = useRecoilValue(soundContextState);
   useJoinZoom(zoomJoinParams);
+  const [volume, setVolumeState] = useState(100);
+  const setVolume = (newVolume: string | number) => {
+    soundContext.setVolume(Number(newVolume) / 100);
+    setVolumeState(Number(newVolume));
+  };
   return (
     <>
       {soundContext.mapSounds((key, name) => (
@@ -22,9 +28,9 @@ export const Meeting = (zoomJoinParams: ZoomJoinParams) => {
         type="range"
         min="0"
         max="100"
-        value="100"
-        onClick={(e) => {
-          soundContext.setVolume(e.currentTarget.valueAsNumber / 100);
+        value={volume}
+        onChange={(e) => {
+          setVolume(e.currentTarget.valueAsNumber);
         }}
       />
     </>
